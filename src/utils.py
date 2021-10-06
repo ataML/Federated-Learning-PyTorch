3d2bc90edbc9debf2fe4f899a4d2c7facc65bf0b
Ata Mazloomian <amazloomian@Atas-MacBook-Air.local>
2021-10-06 12:07:46 +0200

    utils updaated

--- src/utils.py
+++ src/utils.py
@@ -1,75 +1,158 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Python version: 3.6
 
 import copy
 import torch
+import os 
 from torchvision import datasets, transforms
-from sampling import mnist_iid, mnist_noniid, mnist_noniid_unequal
-from sampling import cifar_iid, cifar_noniid
+from sampling import brats_iid
+import numpy as np
+
+from monai.apps import DecathlonDataset
+from monai.data import DataLoader
+
+from monai.transforms import (
+    Activations,
+    AsChannelFirstd,
+    AsDiscrete,
+    CenterSpatialCropd,
+    Compose,
+    LoadImaged,
+    MapTransform,
+    NormalizeIntensityd,
+    Orientationd,
+    RandFlipd,
+    RandScaleIntensityd,
+    RandShiftIntensityd,
+    RandSpatialCropd,
+    Spacingd,
+    ToTensord,
+)
 
 
-def get_dataset(args):
+class ConvertToMultiChannelBasedOnBratsClassesd(MapTransform):
+    """
+    Convert labels to multi channels based on brats classes:
+    label 1 is the peritumoral edema
+    label 2 is the GD-enhancing tumor
+    label 3 is the necrotic and non-enhancing tumor core
+    The possible classes are TC (Tumor core), WT (Whole tumor)
+    and ET (Enhancing tumor).
+
+    """
+
+    def __call__(self, data):
+        d = dict(data)
+        for key in self.keys:
+            result = []
+            # merge label 2 and label 3 to construct TC
+            result.append(np.logical_or(d[key] == 2, d[key] == 3))
+            # merge labels 1, 2 and 3 to construct WT
+            result.append(
+                np.logical_or(
+                    np.logical_or(d[key] == 2, d[key] == 3), d[key] == 1
+                )
+            )
+            # label 2 is ET
+            result.append(d[key] == 2)
+            d[key] = np.stack(result, axis=0).astype(np.float32)
+        return d
+
+def get_dataset(iid, num_users):
     """ Returns train and test datasets and a user group which is a dict where
     the keys are the user index and the values are the corresponding data for
     each of those users.
     """
+    dataset={}
+    user_groups = brats_iid(dataset, num_users)
 
-    if args.dataset == 'cifar':
-        data_dir = '../data/cifar/'
-        apply_transform = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+#    data_dir = '../data/cifar/'
+  #  os.environ["MONAI_DATA_DIRECTORY"]="/data"
+    root_dir = os.environ.get("MONAI_DATA_DIRECTORY")
+    print(root_dir)
+  #   apply_transform = transforms.Compose(
+ #           [transforms.ToTensor(),
+ #            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
-                                       transform=apply_transform)
+     
+    train_transform = Compose(
+        [
+            # load 4 Nifti images and stack them together
+            LoadImaged(keys=["image", "label"]),
+            AsChannelFirstd(keys="image"),
+            ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+            Spacingd(
+                keys=["image", "label"],
+                pixdim=(1.5, 1.5, 2.0),
+                mode=("bilinear", "nearest"),
+            ),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            RandSpatialCropd(
+                keys=["image", "label"], roi_size=[128, 128, 64], random_size=False
+            ),
+            RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
+            NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+            RandScaleIntensityd(keys="image", factors=0.1, prob=0.5),
+            RandShiftIntensityd(keys="image", offsets=0.1, prob=0.5),
+            ToTensord(keys=["image", "label"]),
+        ]
+    )
+    
+    val_transform = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            AsChannelFirstd(keys="image"),
+            ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+            Spacingd(
+                keys=["image", "label"],
+                pixdim=(1.5, 1.5, 2.0),
+                mode=("bilinear", "nearest"),
+            ),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            CenterSpatialCropd(keys=["image", "label"], roi_size=[128, 128, 64]),
+            NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+            ToTensord(keys=["image", "label"]),
+        ]
+    )
+     #   train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
+ #                                      transform=apply_transform)
 
-        test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
-                                      transform=apply_transform)
+ #   test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
+ #                                     transform=apply_transform)
+    train_dataset = DecathlonDataset(
+        root_dir=root_dir,
+        task="Task01_BrainTumour",
+        transform=train_transform,
+        section="training",
+        download=False,
+        num_workers=4,
+        cache_num=100,
+    )
+    
+    val_dataset= DecathlonDataset(
+     
+        root_dir=root_dir,
+        task="Task01_BrainTumour",
+        transform=val_transform,
+        section="validation",
+        download=False,
+        num_workers=4,
+    )
+        
+    
+    if iid:
+        # Sample IID user data from Mnist
+        user_groups_train = brats_iid(train_dataset, num_users)
+        user_groups_val = brats_iid(val_dataset,num_users)
+    
+    #partion files based on user groups
+    #for each partition create separate train and val set
+    #return the list of train-val for each user
+    
+    return train_dataset, val_dataset, user_groups_train, user_groups_val
 
-        # sample training data amongst users
-        if args.iid:
-            # Sample IID user data from Mnist
-            user_groups = cifar_iid(train_dataset, args.num_users)
-        else:
-            # Sample Non-IID user data from Mnist
-            if args.unequal:
-                # Chose uneuqal splits for every user
-                raise NotImplementedError()
-            else:
-                # Chose euqal splits for every user
-                user_groups = cifar_noniid(train_dataset, args.num_users)
-
-    elif args.dataset == 'mnist' or 'fmnist':
-        if args.dataset == 'mnist':
-            data_dir = '../data/mnist/'
-        else:
-            data_dir = '../data/fmnist/'
-
-        apply_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))])
-
-        train_dataset = datasets.MNIST(data_dir, train=True, download=True,
-                                       transform=apply_transform)
-
-        test_dataset = datasets.MNIST(data_dir, train=False, download=True,
-                                      transform=apply_transform)
-
-        # sample training data amongst users
-        if args.iid:
-            # Sample IID user data from Mnist
-            user_groups = mnist_iid(train_dataset, args.num_users)
-        else:
-            # Sample Non-IID user data from Mnist
-            if args.unequal:
-                # Chose uneuqal splits for every user
-                user_groups = mnist_noniid_unequal(train_dataset, args.num_users)
-            else:
-                # Chose euqal splits for every user
-                user_groups = mnist_noniid(train_dataset, args.num_users)
-
-    return train_dataset, test_dataset, user_groups
 
 
 def average_weights(w):
@@ -80,23 +163,6 @@ def average_weights(w):
     for key in w_avg.keys():
         for i in range(1, len(w)):
             w_avg[key] += w[i][key]
+            print("here")
         w_avg[key] = torch.div(w_avg[key], len(w))
     return w_avg
-
-
-def exp_details(args):
-    print('\nExperimental details:')
-    print(f'    Model     : {args.model}')
-    print(f'    Optimizer : {args.optimizer}')
-    print(f'    Learning  : {args.lr}')
-    print(f'    Global Rounds   : {args.epochs}\n')
-
-    print('    Federated parameters:')
-    if args.iid:
-        print('    IID')
-    else:
-        print('    Non-IID')
-    print(f'    Fraction of users  : {args.frac}')
-    print(f'    Local Batch size   : {args.local_bs}')
-    print(f'    Local Epochs       : {args.local_ep}\n')
-    return
