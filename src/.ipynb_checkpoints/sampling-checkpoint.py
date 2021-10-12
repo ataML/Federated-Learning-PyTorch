38ac84a94259eab70014e5207d70c1d444c47dea
root <ata.mazloomian@gmail.com>
2021-10-12 16:03:46 +0000

    package import fix bugs

---
+++ src/.ipynb_checkpoints/sampling-checkpoint.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def brats_iid(dataset, num_users):
+    """
+    Sample I.I.D. client data from Brats dataset
+    :param dataset:
+    :param num_users:
+    :return: dict of image index
+    """
+    num_items = int(len(dataset)/num_users)
+    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    for i in range(num_users):
+        dict_users[i] = set(np.random.choice(all_idxs, num_items,
+                                             replace=False))
+        all_idxs = list(set(all_idxs) - dict_users[i])
+    return dict_users
+
